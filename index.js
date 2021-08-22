9fa05a00e67ebf72b69f23e3bf300b8d202a2f93
“tsige.gidey” <tsige.gidey@yahoo.com>
2021-08-22 11:05:22 -0700

    added viewrs and users

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+
+var express = require('express');
+
+var app = express();
+const PORT = 3000;
+
+app.set('view engine','ejs');//sent views to EJS
+
+//home page
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
+});
+
+//users list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// user/view route - we are cheating by using the array index + 1
+app.get('/users/view/:id', function(req, res) {
+	var title = 'User Page';
+	var id = req.params.id;
+	res.render('users/view', {
+		title: title,
+		user: data[--id]
+	});
+   });
+   
 
+
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+  console.log(data);
+});
 
